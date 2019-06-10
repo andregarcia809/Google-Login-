a7edef7bb73335e42cdc70a5abc137bf058749fd
Andre Garcia <Andres.Garcia@tintworld.com>
2019-06-10 16:59:25 -0400

    Create app directories and files. Added Google+ API Problems with loginUrl variable and config file. Error 500

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+    // session_start();
+    // include dependencies
+    // require_once 'GoogleAPI/vendor/autoload.php';
+
+    // Instantiate  Google Client class
+    // $googleClient = new Google_Client();
+
+    // // App details and API Auth Credentials
+    // $googleClient->setClientId('683353163156-5mn6h9aa3ma617c8mic8arfubc3f641b.apps.googleusercontent.com');
+    // $googleClient->setClientSecret('O6OG4I5_zVSUroeuZTHkkFKK');
+    // $googleClient->setApplicationName('CPI Login Tutorial');
+    // // $googleClient->setRedirectUri('http://devbox.com/googleLogin/google-callback.php');
+    // $googleClient->addScope('https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email');
+ ?>
---
+++ login.php
@@ -0,0 +1,45 @@
+<?Php
+    require_once 'config.php';
+
+    $loginUrl = $googleClient->createAuthUrl();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Log In With Google</title>
+</head>
+
+<body>
+    <div class="container mt-3">
+        <div class="row text-center">
+            <div class="col-md-6">
+                <div class="col-md-6">
+                    <img src="./images/logo.png" class="img-fluid" alt="Logo">
+                </div>
+                <form action="">
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email..">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password..">
+                    </div>
+                    <div class="from-control">
+                        <button class="btn btn-primary">Log In</button>
+                        <button class=" btn btn-danger">Log In
+                            With
+                            Google</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
